faffb63b243b8011edcfcf8fc3e638ffccec2ebf
Violeta Álvarez Areces <violeta.alvarez@volkswagen-groupservices.com>
2025-06-26 10:52:20 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(23)
+}
---
+++ src/test/kotlin/KataTestShould.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class KataTestShould {
+
+    @Test
+    fun `xx`(){
+        assertTrue(true);
+    }
+}
