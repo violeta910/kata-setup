9e21180997550db0c98bd201e3c55804c5956164
Jose A. Villamor <jose.villamor@volkswagen-groupservices.com>
2025-07-02 09:58:57 +0200

    red: player not wins on the first attempt because the random number is higher

--- src/main/kotlin/GuessRandomNumberGame.kt
+++ src/main/kotlin/GuessRandomNumberGame.kt
@@ -1,6 +1,12 @@
 package org.example
 
+import org.example.org.example.RandomNumberGenerator
+
 class GuessRandomNumberGame {
+
+    constructor(randomNumberGenerator: RandomNumberGenerator) {
+
+    }
 
     fun guess(number: Int): String {
         return "Win"
--- src/test/kotlin/GuessRandomNumber.kt
+++ src/test/kotlin/GuessRandomNumber.kt
@@ -1,4 +1,7 @@
+import io.mockk.every
+import io.mockk.mockk
 import org.example.GuessRandomNumberGame
+import org.example.org.example.RandomNumberGenerator
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -8,7 +11,8 @@ class `Guess random number should` {
     @Test
     fun `player wins on the first move`(){
 
-        val game = GuessRandomNumberGame()
+        val stubRandomNumberGenerator = mockk<RandomNumberGenerator>()
+        val game = GuessRandomNumberGame(stubRandomNumberGenerator)
         val expected = "Win"
 
         val actual = game.guess(6)
@@ -18,10 +22,13 @@ class `Guess random number should` {
 
     @Test
     fun `player not wins on the first attempt because the random number is higher`() {
+        val stubRandomNumberGenerator = mockk<RandomNumberGenerator>()
+        every { stubRandomNumberGenerator.generate() } returns 6
 
-        val game = GuessRandomNumberGame()
-        val actual = game.guess(3)
+        val game = GuessRandomNumberGame(stubRandomNumberGenerator)
         val expected = "Higher"
+
+        val actual = game.guess(3)
 
         assertEquals(expected, actual)
     }
