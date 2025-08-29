44f9f7521acdee4c5f470fb6fdf65ee58f1e4d13
Violeta Álvarez Areces <violeta.alvarez@volkswagen-groupservices.com>
2025-08-29 12:01:16 +0200

    red: Given the current time is between 0700 and 0759 when I request the routine activity then the system should display 'Read and study'

--- src/test/kotlin/MorningRoutineShould.kt
+++ src/test/kotlin/MorningRoutineShould.kt
@@ -25,6 +25,22 @@ class MorningRoutineShould {
         // assert
         verify { printerSpy.println("Do exercise")}
     }
+
+    @Test
+    fun `Given the current time is between 0700 and 0759 when I request the routine activity then the system should display 'Read and study'`(){
+        // arrange
+        val clockStub = mockk<Clock>()
+        val printerSpy = spyk<Printer>()
+        val mourningRoutine = MorningRoutine(clockStub, printerSpy)
+
+        every { clockStub.now() } returns LocalTime.of(7, 30)
+
+        // act
+        mourningRoutine.whatShouldIDoNow()
+
+        // assert
+        verify { printerSpy.println("Read and study")}
+    }
 }
 
 
