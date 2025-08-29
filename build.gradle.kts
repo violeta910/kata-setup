60a244d87aaa5d5c74e2de8dd03a73f8da5a1f4d
Emmanuel Valverde Ramos <emmanuel.valverde@volkswagen-groupservices.com>
2025-08-29 08:28:20 +0200

    chore: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+val mockkVersion = "1.14.5"
+val junitVersion = "5.13.0"
 
 repositories {
     mavenCentral()
@@ -11,7 +13,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:${junitVersion}")
+    testImplementation("io.mockk:mockk:${mockkVersion}")
+    testImplementation("io.mockk:mockk-bdd:${mockkVersion}")
 }
 
 tasks.test {
