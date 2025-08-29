dce62b1be286d8a706eccc68f6165d805c73ac9e
Violeta Álvarez Areces <violeta.alvarez@volkswagen-groupservices.com>
2025-08-29 12:03:44 +0200

    green: Given the current time is between 0700 and 0759 when I request the routine activity then the system should display 'Read and study'

--- src/main/kotlin/MorningRoutine.kt
+++ src/main/kotlin/MorningRoutine.kt
@@ -4,6 +4,10 @@ class MorningRoutine(val clock: Clock, val printer: Printer) {
 
     fun whatShouldIDoNow() {
         val currentTime = clock.now()
+        if (currentTime.hour in 7..7) {
+            printer.println("Read and study")
+            return
+        }
         printer.println("Do exercise")
     }
 }
